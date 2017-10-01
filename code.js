46fea4f8871601401f14990bcefb96a986a2bda8
TEDDY STANOWSKI <TSTANOWSKI31@GMAIL.COM>
2017-10-01 12:13:37 -0700

    write variables and methods to create magic eight ball

---
+++ code.js
@@ -0,0 +1,50 @@
+let userName = 'Teddy';
+
+userName ?
+  console.log(`Hello, ${userName}!`)
+	:
+	console.log(`Hello!`);
+
+let userQuestion = "Will I become a millionaire?";
+
+let randomNumber = Math.floor(Math.random() * 7)
+
+let eightBall = '';
+
+switch (randomNumber) {
+  case 0 :
+  	eightBall = 'It is certain';
+ 	  break;
+
+  case 1 :
+  	eightBall = 'It is decidedly so';
+  	break;
+
+  case 2 :
+  	eightBall = 'Reply hazy try again';
+  	break;
+
+  case 3 :
+  	eightBall = 'Cannot predict now';
+  	break;
+
+  case 4 :
+  	eightBall = "Don't count on it";
+  	break;
+
+  case 5 :
+  	eightBall = 'My sources say no';
+  	break ;
+
+  case 6 :
+  	eightBall = 'Outlook not so good';
+  	break;
+
+  case 7 :
+  	eightBall = 'Signs pont to yes';
+  	break;
+
+}
+
+console.log(`${userName}, you asked: ${userQuestion}`);
+console.log(`${eightBall}`);
